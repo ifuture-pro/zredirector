482a3c2c99b7cd713857e0cd32123bb5e7e1b702
ifuture-pro <strongrobot@163.com>
2022-01-25 12:13:29 +0800

    aes

--- crypto/Aes.go
+++ crypto/Aes.go
@@ -1,1 +1,72 @@
 package crypto
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"errors"
+)
+
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+//AES加密,CBC
+func AesEncrypt(origData, key []byte) (cryp []byte, err1 error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err1 = errors.New(x)
+			case error:
+				err1 = x
+			default:
+				err1 = errors.New("caught AesDecrypt Error")
+			}
+		}
+	}()
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData = PKCS7Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+//AES解密
+func AesDecrypt(crypted, key []byte) (cryp []byte, err1 error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch x := r.(type) {
+			case string:
+				err1 = errors.New(x)
+			case error:
+				err1 = x
+			default:
+				err1 = errors.New("caught AesDecrypt Error")
+			}
+		}
+	}()
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS7UnPadding(origData)
+	return origData, nil
+}
